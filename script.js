61518f21a0785ce74241be3bc2694ef8f1f89f38
Alexander Forselius <drsounds@gmail.com>
2012-09-13 14:07:47 +0200

    Simplificating

--- script.js
+++ script.js
@@ -13,18 +13,20 @@ var views = sp.require("sp://import/scripts/api/views");
 var jquery = sp.require("js/jquery-1.7.min");
 var player = models.application.player;
 exports.init = init;
+var current_uri = "";
 var playlist = null;
 var cache = new Array();
+var mode = "user";
 function init(){
 	load();
 }
 function switch_section(section) {
-	$(".section").each(function(index) {
+	$("section").each(function(index) {
 		
 		$(this).hide();
 	});
 
-	document.getElementById("section_" + section).style.display="block";
+	$("#" + section).css("display", "block");
 
 	
 }
@@ -39,60 +41,128 @@ var left = 1220;
 function getLeft(no) {
 	return   window.innerWidth +((no) * tile_size);
 }
-function scrollTo(no) {	
-	$("#l_track").html("");
-	$("#r_track").html("");
-	$("#track").html("");
-	var track = temp_playlist.get(no);
-	// Create first view
-	var player = new views.Image(track.data.album.cover, track.data.uri, "M");
-	player.node.style.width = "340px";
-	player.node.style.height = "340px";
-	
-	$("#track").append(player.node);
-	
-	// Create left and rightview
-	
-	// Create left view
-	if(no > 0) {
-		track = temp_playlist.get(no - 1);
-		var l_player = new views.Image(track.data.album.cover, track.data.uri, "M");
-	
-		l_player.node.style.width = "120px";
-		l_player.node.style.height = "120px";
-		$("#l_track").append(l_player.node);
+function resume() {
+	if(!models.player.playing) {
+		document.getElementById("player").setAttribute("class", "play");
+		models.player.play(temp_playlist.get(pos), temp_playlist);
+	} else {
+		document.getElementById("player").setAttribute("class", "paused");
+		models.player.stop();
 	}
-	if(no < temp_playlist.tracks.length - 1) {
-		track = temp_playlist.get(no+1);
-		var r_player = new views.Image(track.data.album.cover, track.data.uri, "M");
-	
-		r_player.node.style.width = "120px";
-		r_player.node.style.height = "120px";
-		$("#r_track").append(r_player.node);
-	}
-	
 }
-var mode = "user";
+function scrollTo(no) {	
+	try {
+		pos = no;
+		var track = temp_playlist.get(no);
+		console.log($(".cover"));
+		// Shift the others to the left
+		$(".cover").each(function(index) {
+			console.log("Aw");
+			$(this).animate({left:"-=155px", top:"+=15px", height: "-=30px", width:"-=35px", opacity: "-=0.25"});
+			if($(this).css("opacity") < 0.25) {
+				$(this).remove();
+			}
+		});
+		// Create first view
+		var player = new views.Image(track.data.album.cover, track.data.uri, "M");
+		player.node.style.width = "252px";
+		player.node.style.height = "252px";
+		player.node.style.position = "absolute";
+		player.node.style.left = "320px";
+		player.node.style.top = "120px";
+		console.log(player.node);
+		$(player.node).addClass("cover");
+		
+		document.getElementById("radiostream").appendChild(player.node);
+		console.log($("#radiostram"));
+		$(player.node).fadeIn();
+		$("#player").css({ width: 79, height: 79, "z-index" : 11});
+		
+	} catch (e) {
+		console.log(e);
+	}	
+}
+function prepareShowQuery(str, date) {
+	if(typeof date == "undefined") {
+		date = new Date();
+	}
+	str = str.replace("%d", date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate());
+	str = str.replace("%t", date.getHours()+ ":" + date.getMinutes());
+	return str;
+}
+var playlist = "";
+var radio_pos = 0;
+var show_queries = ["Deluxe  P3", "Sommar P1", "Mammas nya kille P3"];
+var show = false;	
+var users = "";
+var pos = 0;
 var playlist = null;
 function scrobble() {
 	if(!activated) {
 		return;
 	}
 	var users = [user];
+	if(user == "$me") {
+		var friends = ["drsounds"];
+	}
+	console.log(user);
 	if(user!=null) {
 		if(user.indexOf(",")!=-1) {
 			users = user.split(",");
+			
 		}
 	}
-	
+	var found = false;
+	// Invalidate all classes
+	$(".user").each(function(index) {
+		$(this).removeClass("playing");
+		console.log("REMOVED");
+	});
+	var date = new Date();
 	for(var i = 0; i < 5; i++) {
 		// Get toplist of user
 		var  toplist = new models.Toplist();
-		// Select mode by random
+		// Select mode by random 
+		if(date.getMinutes() >= 0 && date.getMinutes() <= 5 && show && show_queries.length > 0 ) {
+			if(radio_pos > show_queries.length -1) {
+				radio_pos = 0;
+			}
+			var show_query = show_queries[radio_pos];
+			radio_pos++;
+			console.log(show_query);
+			// If the show query is a sptoify uri use it, otherwise search it like a string
+			if(show_query.indexOf("spotify:track") == -1) {
+				var search = new models.Search(prepareShowQuery(show_query, date), {localResults: models.LOCALSEARCHRESULTS.PREPEND});
+				search.observe(models.EVENT.CHANGE, function() {
+					console.log(search.tracks.length);
+					if(search.tracks.length > 0) {
+					
+						var track2 = search.tracks[0];
+						console.log(track2);
+						temp_playlist.add(track2);
+						found =true;
+						if(temp_playlist.length > 0) {
+							if(first) {
+								scrollTo(0);
+								//models.player.play(temp_playlist.get(0), temp_playlist);
+								first = false;
+								
+							}
+						} else {
+						}
+					}
+				});
+				search.appendNext();
+			} else {
+				models.Track.fromURI(show_query, function(track) {
+					temp_playlist.add(track);
+				});
+			}
+		}
 		if(mode == "playlist") {
 			var pls = models.Playlist.fromURI("spotify:user:" + user + ":playlist:" + playlist, function(playlist) {
 				var track = playlist.tracks[Math.floor(playlist.tracsk.length*Math.random())];
-				var search = new models.Search("artist:\"" + track.data.artists[0].uri +"\"");
+				var search = new models.Search("artist:\"" + track.data.artists[0].uri +"\"", { "pageSize": 120, "searchTracks": true, "searchPlaylists": false, "searchArtists": false});
 				search.observe(modes.EVENT.CHANGE, function() {
 					var track2 = search.tracks[Math.floor(search.tracks.length*Math.random())];
 					temp_playlist.add(track2);
@@ -103,6 +173,7 @@ function scrobble() {
 							first = false;
 						}
 					} else {
+						
 					}
 				});
 				search.appendNext();
@@ -115,7 +186,9 @@ function scrobble() {
 					toplist.matchType = models.TOPLISTMATCHES.TRACKS; 
 					toplist.userName = users[Math.floor(users.length*Math.random())];
 					toplist.observe(models.EVENT.CHANGE, function() {
+					
 						console.log(Math.floor(toplist.results.length*Math.random()));
+						
 						var _track = toplist.results[Math.floor(toplist.results.length*(1-Math.random()))];
 							console.log(_track.data);	
 							console.log(_track.data.artists[0].uri);
@@ -124,8 +197,10 @@ function scrobble() {
 							console.log(artist.data.name);
 							var search = new models.Search("artist:\"" + artist.data.name + "\"");
 							search.observe(models.EVENT.CHANGE, function() {
+								
 								var track = search.tracks[Math.floor(search.tracks.length*Math.random())];
 								temp_playlist.add(track);
+								found = true;
 								if(temp_playlist.length > 0) {
 									if(first) {
 										scrollTo(0);
@@ -133,7 +208,10 @@ function scrobble() {
 										first = false;
 									}
 								} else {
+									
 								}
+								
+								switch_section("radio");
 							});
 							search.appendNext();
 						});
@@ -187,6 +265,11 @@ function scrobble() {
 			}
 		}
 	}
+
+	
+	switch_section("sorry");
+	
+	
 }
 var genre ="";
 var user = "";
@@ -200,17 +283,101 @@ function addPlaylist() {
 		playlist.add(track);
 	});
 }
+function update() {
+	try {
+		document.body.style.backgroundImage = "";
+		activated = true;
+		var args = models.application.arguments;
+		console.log(args);
+		temp_playlist = new models.Playlist();
+		
+		first = true;
+		
+		if(args.length > 1) {
+			
+			mode = args[0];
+			user = args[1];
+			
+			
+			var view = args[args.length-1];
+			if(typeof(view) === "undefined") {
+				view = "overview";
+			}
+			console.log("F");
+			// Create user 
+			var users = [user];
+			console.log(user);
+			if(mode == "user") {
+				if(user.indexOf(",")!=-1) {
+					users = user.split(",");
+				}
+				var nav = document.getElementById("users_list");
+				$(nav).html(""); // Clear
+				
+				for(var i = 0; i < users.length; i++) {	
+					console.log("USERS");
+					var li = document.createElement("li");
+					
+					$(li).addClass("user");
+					
+					li.setAttribute("id", "u_" + users[i]);
+					var a = document.createElement("a");
+					a.setAttribute("href", "spotify:user:" + users[i]);
+					$(a).html(users[i]);
+					li.appendChild(a);
+					$("#users_list").append(li);
+				}
+			
+				
+				scrobble();
+			}
+			
+		} else {
+			//alert("A");
+			switch_section("overview");
+		}
+		/*else {
+			user = "$me";
+			var li = document.createElement("li");
+			
+			$(li).addClass("user");
+			
+			li.setAttribute("id", "u_friends");
+			var a = document.createElement("a");
+			a.setAttribute("href", "spotify:app:people:spotify");
+			$(a).html("Me");
+			li.appendChild(a);
+			$("#users_list").append(li);
+		//	switch_section("overview");
+		}*/
+	} catch( e) {
+		
+		console.log(e.stack);
+	}
+}
 function load(){
+	$("#overview").show();
+	console.log(sp.social);
+	console.log(sp);
 	document.getElementById("addPlaylist").addEventListener("click", addPlaylist);
-	console.log(models.EVENT);
+	
 	temp_playlist = new models.Playlist();
-
+	document.getElementById("player").addEventListener("click", function() {
+		if(!models.player.playing) {	
+			document.getElementById("player").setAttribute("class", "play");
+			models.player.playing = true;
+			
+		} else {
+			document.getElementById("player").setAttribute("class", "paused");
+			models.player.playing = false;
+		}
+	});
 	$("#btnGo").bind("click", function(e) {
 		var user = document.getElementById("user").value;
 
 		self.location="spotify:app:jukeman:" + user ;
 	});
-
+	update();
 	models.application.observe(models.EVENT.ACTIVATE, function() {
 		activated = true;
 	});
@@ -218,41 +385,20 @@ function load(){
 		activated = false;
 	});
 	models.application.observe(models.EVENT.ARGUMENTSCHANGED, function() {
-		document.body.style.backgroundImage = "";
-		activated = true;
-		var args = models.application.arguments;
-		console.log(args);
-		
-		
-		first = true;
-		
-		try {
-			
-			if(args.length > 0) {
-				mode = args[0];
-				user = args[1];
-				mode = "user";
-				if(args.length > 2) {
-					playlist = args[3];
-					mode = "playlist";
-				}
-				$("#username").html(user);
-		//		switch_section("radio");
-				scrobble();
-			} else {
-				$("#username").html("Jukeman");
-			//	switch_section("overview");
-			}
-		} catch( e) {
-			
-			console.log(e.stack);
-		}
+		current_uri = self.location.href;
+		update();
 	});
 	
 	models.player.observe(models.EVENT.CHANGE, function(event) {
 		
-		if(temp_playlist == null)
-			return;
+		if(!models.player.playing) {
+			document.getElementById("player").setAttribute("class", "play");
+		
+		} else {
+			document.getElementById("player").setAttribute("class", "paused");
+		
+		}
+		
 		
 		if(event.data.curtrack) {
 			console.log("F");
@@ -265,5 +411,21 @@ function load(){
 			scrollTo(pos);
 		}
 	});
-	
-}
+	var dropzone = document.querySelector('#dropzone');
+	dropzone.addEventListener('dragover', handleDragOver, false);
+	dropzone.addEventListener('drop', handleDrop, false);
+
+	function handleDragOver(e) {
+		e.preventDefault();
+		e.dataTransfer.dropEffect = 'copy';
+		return false;
+	}
+
+	function handleDrop(e) {
+		var droppedURI = e.dataTransfer.getData('Text');
+		droppedURI = droppedURI.replace("http://open.spotify.com/", "spotify:");
+		droppedURI = droppedURI.replace(/\//i, ":");
+		droppedURI = droppedURI.replace("spotify:user:", "spotify:app:jukeman:user:");
+		self.location = droppedURI + ":radio";
+	}
+}	
